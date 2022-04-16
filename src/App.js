9d0615fbad23afdadccd65230dffc3351fcdd743
Ali Naderlou <alinaderlo76@gmail.com>
2022-04-16 19:14:23 +0430

    header completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import './normalize.css'
+import Header from "./Components/Header/Header";
 
 function App() {
     return (
-        <div>
-
-        </div>
+        <section>
+            <Header/>
+        </section>
     );
 }
 
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import './Header.scss'
+import {FaSearch} from 'react-icons/fa';
+
+const Header = () => {
+    const [searchValue, setSearchValue] = useState('')
+    return (
+        <header className="header-wrapper">
+            <div className="logo-wrapper">
+                <img className="logo" src="/Images/logo.png" alt="red-wolf-gallery"/>
+            </div>
+            <div className="search-wrapper">
+                <div className="search">
+                    <div className="icon">
+                        <FaSearch/>
+                    </div>
+                    <input
+                        type="text"
+                        value={searchValue}
+                        placeholder="Search photo"
+                        onChange={(e => {
+                            setSearchValue(e.target.value)
+                        })}/>
+                </div>
+            </div>
+            <div className="nav-items-wrapper">
+                <ul className="nav-items">
+                    <li className="nav-item">
+                        Home
+                    </li>
+                    <li className="nav-item">
+                        Blog
+                    </li>
+                    <li className="nav-item">
+                        Contact Us
+                    </li>
+                </ul>
+                <div className="auth-wrapper">
+                    <div>Login</div>
+                    <span className="separator">/</span>
+                    <div>Sign up</div>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
